eccd5f3fc324bebab503a3cad73c2348ad530761
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-03-17 11:10:15 +0530

    Establish the database connection

---
+++ databaseConnections/sequelizeDatabaseConnection.ts
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+import * as dotenv from 'dotenv'
+dotenv.config({
+    override: true,
+    path: '.env',
+  });
+
+const database = process.env.DATABASE_NAME|| ''
+const username = process.env.DATABASE_USERNAME || '';
+const password = process.env.DATABASE_PASSWORD || '';
+const host = process.env.DATABASE_HOST || '';
+const dialect = process.env.DATABASE_DIALECT
+
+export const sequelizeConnection = new Sequelize(database, username, password, {
+    host: host,
+    dialect: dialect,
+});
