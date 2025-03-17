c6819d8c8fbda4eb54f0b47bc9c9f7e718e584b2
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-03-17 18:04:43 +0530

    Update the database connection

--- databaseConnections/sequelizeDatabaseConnection.ts
+++ databaseConnections/sequelizeDatabaseConnection.ts
@@ -5,13 +5,19 @@ dotenv.config({
     path: '.env',
   });
 
-const database = process.env.DATABASE_NAME|| ''
-const username = process.env.DATABASE_USERNAME || '';
-const password = process.env.DATABASE_PASSWORD || '';
-const host = process.env.DATABASE_HOST || '';
+const database = process.env.DATABASE_NAME|| 'database-2'
+const username = process.env.DATABASE_USERNAME || 'cloudadventure';
+const password = process.env.DATABASE_PASSWORD || 'vinaysai';
+const host = process.env.DATABASE_HOST || 'database-2.cja0gy6g8226.ap-south-1.rds.amazonaws.com';
 const dialect = process.env.DATABASE_DIALECT || 'postgres'
 
 export const sequelizeConnection = new Sequelize(database, username, password, {
     host: host,
     dialect: dialect,
+    dialectOpitions:{
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
 });
